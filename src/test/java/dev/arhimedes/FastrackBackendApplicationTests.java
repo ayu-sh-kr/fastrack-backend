2c80f2200aa90f32933bb2ff2183f4e04eb99027
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-08 22:16:50 +0530

    Initial Commit

---
+++ src/test/java/dev/arhimedes/FastrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arhimedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
