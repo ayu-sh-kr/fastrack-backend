9bc30967f6acaa8f9cab613cfa885da858936944
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-10 20:38:13 +0530

    ExcelColumn annotation provide meta data
    It is used to map Java object field to Excel sheet column

---
+++ src/main/java/dev/arhimedes/excel/utils/annotation/ExcelColumn.java
@@ -0,0 +1,14 @@
+package dev.arhimedes.excel.utils.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcelColumn {
+    String name() default "";
+
+    int cellNumber() default -1;
+}
